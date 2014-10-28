2e28e96e9cd5e6cb7337dbb7a7f1c1ab3dda746b
tkpeck <TPeck@Toris-MacBook-Pro.local>
2014-10-28 13:43:15 -0400

    started demo

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Animated Page Scroll
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
+// bigtext.js
+$(function() {
+    WebFont.load({
+        custom: {
+            families: ['Yanone Kaffeesatz'], // font-family name
+            urls : ['css/main.css'] // URL to css
+        },
+        active: function() {
+            $('#bigtext').bigtext();
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Project & Demo Boilerplate/Framework
+As you start a new demo or project you should use this boilerplate/framework as a beginning point. As you learn more throughout the semester new features will be added to the boilerplate/framework.
+
+## Resources
+### Responsive Frameworks
+- <a href="http://getbootstrap.com/" target="_blank" title="Boostrap">Boostrap</a>
+- <a href="http://foundation.zurb.com/" target="_blank" title="Zurb Foundation">Zurb Foundation</a>
+- <a href="http://gumbyframework.com/" target="_blank" title="Gumby">Gumby</a>
